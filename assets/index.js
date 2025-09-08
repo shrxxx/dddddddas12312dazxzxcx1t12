a02bba642414ac29a4759a4c18ebc93dcb4b0b4e
shrxxx <111366160+shrxxx@users.noreply.github.com>
2025-09-08 21:39:20 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,134 +1,99 @@
-<head>
-    <link rel="stylesheet" href="assets/index.css">
-    <link rel="icon" type="image/x-icon"
-        href="https://play-lh.googleusercontent.com/_TNbiYKasPy_isTSEg2_UEzVaev4F8fO2lLunsHJ8_Ux2g3OzkSZyzpqvJG1woSj-WD4=w240-h480-rw">
-    <title>mObywatel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no">
-</head>
+const upload = document.querySelector('.upload');
+const imageInput = document.createElement('input');
 
-<body>
-    <img src="https://play-lh.googleusercontent.com/_TNbiYKasPy_isTSEg2_UEzVaev4F8fO2lLunsHJ8_Ux2g3OzkSZyzpqvJG1woSj-WD4"
-        class="logo">
-    <p class="logo_text">FistaszjoObywatel 4.2.0</p>
+imageInput.type = 'file';
+imageInput.accept = '.jpeg,.png,.gif';
 
-    <div class="guide_holder">
-        <div class="top_holder">
-            <p class="guide_title">Instrukcja</p>
-            <img class="arrow" src="https://i.imgur.com/aUZmu0W.png">
-        </div>
-        <div class="guide_text">
-            <span style="font-size: 17px; font-weight: 500;">Dla systemu IOS:</span><br>
-            <ul>
-                <li>Upewnij się, że używasz przeglądarki Safari</li>
-                <li>Uzupełnij dane i kliknij wejdź</li>
-                <li>Naciśji udostępnij -> Do Ekranu głównego</li>
-            </ul>
-            <span style="font-size: 17px; font-weight: 500;">Dla systemu Android:</span>
-            <ul>
-                <li>Upewnij się, że używasz przeglądarki google lub chrome</li>
-                <li>Uzupełnij dane i kliknij wejdź</li>
-                <li>Naciśji 3 kropki -> Dodaj do ekranu głównego</li>
-            </ul>
-        </div>
-    </div>
+document.querySelectorAll('.input_holder').forEach((element) => {
+  const input = element.querySelector('.input');
+  input.addEventListener('click', () => {
+    element.classList.remove('error_shown');
+  });
+});
 
-    <div class="upload">
-        <p class="error">To pole jest wymagane</p>
-        <img class="upload_uploaded">
-        <div class="upload_uploading"></div>
-        <div class="upload_grid">
-            <img class="upload_image" src="https://i.imgur.com/vwIQErh.png">
-            <p class="upload_text">Dodaj zdjęcie</p>
-        </div>
-    </div>
+upload.addEventListener('click', () => imageInput.click());
 
-    <div class="input_holder">
-        <input type="text" id="name" class="input" placeholder="">
-        <span class="placeholder">Imię (imiona)</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+imageInput.addEventListener('change', async () => {
+  resetUploadState();
 
-    <div class="input_holder">
-        <input type="text" id="surname" class="input" placeholder="">
-        <span class="placeholder">Nazwisko</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+  const file = imageInput.files[0];
+  if (!file) return;
 
-    <div class="input_holder">
-        <input type="text" id="sex" class="input" placeholder="">
-        <span class="placeholder">Płeć</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+  const formData = new FormData();
+  formData.append('image', file);
 
-    <div class="input_holder">
-        <input type="text" id="nationality" class="input" placeholder="">
-        <span class="placeholder">Obywatelstwo</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+  try {
+    const response = await fetch('https://api.imgur.com/3/image', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Client-ID 774f3ba80197c47',
+      },
+      body: formData,
+    });
+    const result = await response.json();
 
-    <div class="input_holder">
-        <input type="text" id="birthday" class="input" placeholder="">
-        <span class="placeholder">Data urodzenia (DD.MM.YYYY)</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+    if (result?.data?.link) {
+      updateUploadState(result.data.link);
+    } else {
+      showErrorState();
+    }
+  } catch {
+    showErrorState();
+  }
+});
 
-    <div class="input_holder">
-        <input type="text" id="familyName" class="input" placeholder="">
-        <span class="placeholder">Nazwisko rodowe</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+document.querySelector('.go').addEventListener('click', () => {
+  const emptyFields = [];
+  const params = new URLSearchParams();
 
-    <div class="input_holder">
-        <input type="text" id="fathersFamilyName" class="input" placeholder="">
-        <span class="placeholder">Nazwisko rodowe ojca</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+  if (!upload.hasAttribute('selected')) {
+    emptyFields.push(upload);
+    upload.classList.add('error_shown');
+  } else {
+    params.append('image', upload.getAttribute('selected'));
+  }
 
-    <div class="input_holder">
-        <input type="text" id="mothersFamilyName" class="input" placeholder="">
-        <span class="placeholder">Nazwisko rodowe matki</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+  document.querySelectorAll('.input_holder').forEach((element) => {
+    const input = element.querySelector('.input');
+    params.append(input.id, input.value);
 
-    <p class="subtext">Miejsce zamieszkania</p>
+    if (isEmpty(input.value)) {
+      emptyFields.push(element);
+      element.classList.add('error_shown');
+    }
+  });
 
-    <div class="input_holder">
-        <input type="text" id="birthPlace" class="input" placeholder="">
-        <span class="placeholder">Miejsce urodzenia</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+  if (emptyFields.length > 0) {
+    emptyFields[0].scrollIntoView();
+  } else {
+    forwardToId(params);
+  }
+});
 
-    <div class="input_holder">
-        <input type="text" id="countryOfBirth" class="input" placeholder="">
-        <span class="placeholder">Kraj urodzenia</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+function isEmpty(value) {
+  return /^\s*$/.test(value);
+}
 
-    <div class="input_holder">
-        <input type="text" id="adress1" class="input" placeholder="">
-        <span class="placeholder">Ulica i numer domu</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+function resetUploadState() {
+  upload.classList.remove('upload_loaded', 'upload_loading', 'error_shown');
+  upload.removeAttribute('selected');
+}
 
-    <div class="input_holder">
-        <input type="text" id="adress2" class="input" placeholder="">
-        <span class="placeholder">Kod pocztowy</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+function updateUploadState(url) {
+  upload.classList.add('upload_loaded');
+  upload.setAttribute('selected', url);
+  upload.querySelector('.upload_uploaded').src = url;
+}
 
-    <div class="input_holder">
-        <input type="text" id="city" class="input" placeholder="">
-        <span class="placeholder">Miasto</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+function showErrorState() {
+  upload.classList.add('error_shown');
+}
 
-    <div class="input_holder">
-        <input type="text" id="checkInDate" class="input" placeholder="">
-        <span class="placeholder">Data zameldowania na pobyt stały (DD.MM.YYYY)</span>
-        <p class="error">To pole jest wymagane</p>
-    </div>
+function forwardToId(params) {
+  location.href = `/FistaszjoObywatel/id?${params}`;
+}
 
-    <a class="go">wejdź</a>
-
-    <script src="assets/index.js"></script>
-</body>
+const guide = document.querySelector('.guide_holder');
+guide.addEventListener('click', () => {
+  guide.classList.toggle('unfolded');
+});
