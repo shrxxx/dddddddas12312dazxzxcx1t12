fe5b1b1785c70c5636b952cad25de9c51f05c301
shrxxx <111366160+shrxxx@users.noreply.github.com>
2025-09-08 21:39:04 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,158 +1,134 @@
-var selector = document.querySelector(".selector_box");
-if (selector) {
-    selector.addEventListener('click', () => {
-        if (selector.classList.contains("selector_open")) {
-            selector.classList.remove("selector_open")
-        } else {
-            selector.classList.add("selector_open")
-        }
-    })
-}
+<head>
+    <link rel="stylesheet" href="assets/index.css">
+    <link rel="icon" type="image/x-icon"
+        href="https://play-lh.googleusercontent.com/_TNbiYKasPy_isTSEg2_UEzVaev4F8fO2lLunsHJ8_Ux2g3OzkSZyzpqvJG1woSj-WD4=w240-h480-rw">
+    <title>mObywatel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no">
+</head>
 
+<body>
+    <img src="https://play-lh.googleusercontent.com/_TNbiYKasPy_isTSEg2_UEzVaev4F8fO2lLunsHJ8_Ux2g3OzkSZyzpqvJG1woSj-WD4"
+        class="logo">
+    <p class="logo_text">FistaszjoObywatel 4.2.0</p>
 
-document.querySelectorAll(".date_input").forEach((element) => {
-    element.addEventListener('click', () => {
-        document.querySelector(".date").classList.remove("error_shown")
-    })
-})
+    <div class="guide_holder">
+        <div class="top_holder">
+            <p class="guide_title">Instrukcja</p>
+            <img class="arrow" src="https://i.imgur.com/aUZmu0W.png">
+        </div>
+        <div class="guide_text">
+            <span style="font-size: 17px; font-weight: 500;">Dla systemu IOS:</span><br>
+            <ul>
+                <li>Upewnij się, że używasz przeglądarki Safari</li>
+                <li>Uzupełnij dane i kliknij wejdź</li>
+                <li>Naciśji udostępnij -> Do Ekranu głównego</li>
+            </ul>
+            <span style="font-size: 17px; font-weight: 500;">Dla systemu Android:</span>
+            <ul>
+                <li>Upewnij się, że używasz przeglądarki google lub chrome</li>
+                <li>Uzupełnij dane i kliknij wejdź</li>
+                <li>Naciśji 3 kropki -> Dodaj do ekranu głównego</li>
+            </ul>
+        </div>
+    </div>
 
-var sex = "m"
+    <div class="upload">
+        <p class="error">To pole jest wymagane</p>
+        <img class="upload_uploaded">
+        <div class="upload_uploading"></div>
+        <div class="upload_grid">
+            <img class="upload_image" src="https://i.imgur.com/vwIQErh.png">
+            <p class="upload_text">Dodaj zdjęcie</p>
+        </div>
+    </div>
 
-document.querySelectorAll(".selector_option").forEach((option) => {
-    option.addEventListener('click', () => {
-        sex = option.id;
-        document.querySelector(".selected_text").innerHTML = option.innerHTML;
-    })
-})
+    <div class="input_holder">
+        <input type="text" id="name" class="input" placeholder="">
+        <span class="placeholder">Imię (imiona)</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-var upload = document.querySelector(".upload");
+    <div class="input_holder">
+        <input type="text" id="surname" class="input" placeholder="">
+        <span class="placeholder">Nazwisko</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-// Select the image input from the HTML
-var imageInput = document.getElementById("imageInput");
+    <div class="input_holder">
+        <input type="text" id="sex" class="input" placeholder="">
+        <span class="placeholder">Płeć</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-document.querySelectorAll(".input_holder").forEach((element) => {
+    <div class="input_holder">
+        <input type="text" id="nationality" class="input" placeholder="">
+        <span class="placeholder">Obywatelstwo</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-    var input = element.querySelector(".input");
-    input.addEventListener('click', () => {
-        element.classList.remove("error_shown");
-    })
+    <div class="input_holder">
+        <input type="text" id="birthday" class="input" placeholder="">
+        <span class="placeholder">Data urodzenia (DD.MM.YYYY)</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-});
+    <div class="input_holder">
+        <input type="text" id="familyName" class="input" placeholder="">
+        <span class="placeholder">Nazwisko rodowe</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-upload.addEventListener('click', () => {
-    imageInput.click();
-    upload.classList.remove("error_shown")
-});
+    <div class="input_holder">
+        <input type="text" id="fathersFamilyName" class="input" placeholder="">
+        <span class="placeholder">Nazwisko rodowe ojca</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-imageInput.addEventListener('change', (event) => {
+    <div class="input_holder">
+        <input type="text" id="mothersFamilyName" class="input" placeholder="">
+        <span class="placeholder">Nazwisko rodowe matki</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-    upload.classList.remove("upload_loaded");
-    upload.classList.add("upload_loading");
+    <p class="subtext">Miejsce zamieszkania</p>
 
-    upload.removeAttribute("selected")
+    <div class="input_holder">
+        <input type="text" id="birthPlace" class="input" placeholder="">
+        <span class="placeholder">Miejsce urodzenia</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-    var file = imageInput.files[0];
-    var data = new FormData();
-    data.append("image", file);
+    <div class="input_holder">
+        <input type="text" id="countryOfBirth" class="input" placeholder="">
+        <span class="placeholder">Kraj urodzenia</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-    fetch('https://api.imgur.com/3/image', {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Client-ID ec67bcef2e19c08'
-        },
-        body: data
-    })
-        .then(result => result.json())
-        .then(response => {
+    <div class="input_holder">
+        <input type="text" id="adress1" class="input" placeholder="">
+        <span class="placeholder">Ulica i numer domu</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-            var url = response.data.link;
-            upload.classList.remove("error_shown")
-            upload.setAttribute("selected", url);
-            upload.classList.add("upload_loaded");
-            upload.classList.remove("upload_loading");
-            upload.querySelector(".upload_uploaded").src = url;
+    <div class="input_holder">
+        <input type="text" id="adress2" class="input" placeholder="">
+        <span class="placeholder">Kod pocztowy</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-        })
+    <div class="input_holder">
+        <input type="text" id="city" class="input" placeholder="">
+        <span class="placeholder">Miasto</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-})
+    <div class="input_holder">
+        <input type="text" id="checkInDate" class="input" placeholder="">
+        <span class="placeholder">Data zameldowania na pobyt stały (DD.MM.YYYY)</span>
+        <p class="error">To pole jest wymagane</p>
+    </div>
 
-document.querySelector(".go").addEventListener('click', () => {
+    <a class="go">wejdź</a>
 
-    var empty = [];
-
-    var params = new URLSearchParams();
-
-    params.set("sex", sex)
-    if (!upload.hasAttribute("selected")) {
-        empty.push(upload);
-        upload.classList.add("error_shown")
-    } else {
-        params.set("image", upload.getAttribute("selected"))
-    }
-
-    var birthday = "";
-    var dateEmpty = false;
-    document.querySelectorAll(".date_input").forEach((element) => {
-        birthday = birthday + "." + element.value
-        if (isEmpty(element.value)) {
-            dateEmpty = true;
-        }
-    })
-
-    birthday = birthday.substring(1);
-
-    if (dateEmpty) {
-        var dateElement = document.querySelector(".date");
-        dateElement.classList.add("error_shown");
-        empty.push(dateElement);
-    } else {
-        params.set("birthday", birthday)
-    }
-
-    document.querySelectorAll(".input_holder").forEach((element) => {
-
-        var input = element.querySelector(".input");
-
-        if (isEmpty(input.value)) {
-            empty.push(element);
-            element.classList.add("error_shown");
-        } else {
-            params.set(input.id, input.value)
-        }
-
-    })
-
-    if (empty.length != 0) {
-        empty[0].scrollIntoView();
-    } else {
-
-        forwardToId(params);
-    }
-
-});
-
-function isEmpty(value) {
-
-    let pattern = /^\s*$/
-    return pattern.test(value);
-
-}
-
-function forwardToId(params) {
-
-    location.href = "id.html" + params
-
-}
-
-var guide = document.querySelector(".guide_holder");
-if (guide) {
-    guide.addEventListener('click', () => {
-
-        if (guide.classList.contains("unfolded")) {
-            guide.classList.remove("unfolded");
-        } else {
-            guide.classList.add("unfolded");
-        }
-
-    })
-}
+    <script src="assets/index.js"></script>
+</body>
